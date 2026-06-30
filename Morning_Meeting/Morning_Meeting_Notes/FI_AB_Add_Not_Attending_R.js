6f465bdf2e0169c3101999ff2f5195ffd8ee0fd8
abhishek-fi <abhishek@fristinetech.com>
2026-06-30 18:48:40 +0530

    Create FI_AB_Add_Not_Attending_R.js

---
+++ Morning_Meeting/Morning_Meeting_Notes/FI_AB_Add_Not_Attending_R.js
@@ -0,0 +1,22 @@
+// Created - Successful form submission - FI_AB Add Not Attending Reason to Master
+if(input.Have_you_attended_today_s_meeting == "No" && input.Meeting_Detail != null)
+{
+	masterRec = Morning_Meeting_Notes_Master[Meeting_Detail == input.Meeting_Detail];
+	if(masterRec.count() == 0)
+	{
+		masterId = insert into Morning_Meeting_Notes_Master
+		[
+			Added_User=zoho.loginuser
+			Meeting_Detail=input.Meeting_Detail
+		];
+		masterRec = Morning_Meeting_Notes_Master[ID == masterId];
+	}
+	for each  rec in masterRec
+	{
+		tempsList = Collection();
+		tempsList.insertall(rec.Not_Attended_List);
+	}
+	// 	existingList=masterRec.Not_Attended_List.getal();
+	tempsList.insert(input.ID);
+	masterRec.Not_Attended_List=tempsList;
+}
