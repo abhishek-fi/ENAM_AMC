91dc0e6dedabdf99899bccd2a5e598b5bf7d3354
abhishek-fi <abhishek@fristinetech.com>
2026-06-30 18:47:01 +0530

    Create No_one_to_be_create_recor.js

---
+++ Morning_Meeting/Morning_Meeting_Notes/No_one_to_be_create_recor.js
@@ -0,0 +1,11 @@
+// Created - Load of the form - No one to be create record from the form
+disable Have_you_attended_today_s_meeting;
+disable Modification_Submitted;
+disable Approver_Name;
+disable Submission_Remarks;
+disable Status;
+disable Analyst;
+disable Meeting_Detail;
+disable Mode;
+disable Not_attended_reason;
+alert "You do not have access to create record!";
