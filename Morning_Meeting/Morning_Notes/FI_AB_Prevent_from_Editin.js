957be63a111e3ae18529a61b460d33edcc16f77d
abhishek-fi <abhishek@fristinetech.com>
2026-06-30 18:16:21 +0530

    Create FI_AB_Prevent_from_Editin.js

---
+++ Morning_Meeting/Morning_Notes/FI_AB_Prevent_from_Editin.js
@@ -0,0 +1,5 @@
+// Edited - Load of the form - FI_AB Prevent from Editing
+if(input.Edit_Access == false && Record_Status1 == "Stored")
+{
+	openUrl("#Script:page.parent.refresh ","parent window");
+}
