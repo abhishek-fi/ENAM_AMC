51147ec239f6ca142db4e79cf88b59042c938c0e
abhishek-fi <abhishek@fristinetech.com>
2026-06-30 18:51:03 +0530

    Create FI_AB_Disable_fields_whil.js

---
+++ Morning_Meeting/Morning_Meeting_Notes/FI_AB_Disable_fields_whil.js
@@ -0,0 +1,7 @@
+// Edited - Load of the form - FI_AB Disable fields while editing
+disable Meeting_Detail;
+disable Have_you_attended_today_s_meeting;
+disable Mode;
+disable Not_attended_reason;
+disable Analyst;
+disable Status;
