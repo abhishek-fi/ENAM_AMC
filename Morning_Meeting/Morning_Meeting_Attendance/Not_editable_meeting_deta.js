f773fed22705c874c604f544077c23e905911436
abhishek-fi <abhishek@fristinetech.com>
2026-06-30 17:29:31 +0530

    Create Not_editable_meeting_deta.js

---
+++ Morning_Meeting/Morning_Meeting_Attendance/Not_editable_meeting_deta.js
@@ -0,0 +1,6 @@
+// Created or Edited - Load of the form - Not editable meeting details and Analyst
+if(zoho.loginuserid != "zoho@enamamc.com")
+{
+	disable Meeting_Detail;
+	disable Analyst_Name;
+}
