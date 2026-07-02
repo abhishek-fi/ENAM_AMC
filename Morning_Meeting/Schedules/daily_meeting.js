ce7c3940c866febb9c402867c99baab9a15ce89f
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 12:24:19 +0530

    Update daily_meeting.js

--- Morning_Meeting/Schedules/daily_meeting.js
+++ Morning_Meeting/Schedules/daily_meeting.js
@@ -1,4 +1,4 @@
-// On 30-Jul-2025 Daily - At 11:30:00 - daily meeting
+// On 30-Jul-2025 - At 11:30:00 Daily - daily meeting
 /* tomorrow = zoho.currentdate.addDay(1);
  				 														day = tomorrow.getDayOfWeek();
  				 														if(day == 1 || day == 7)
