809e69ab9e49d2670c50979fa5d8889d7e420631
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 02:08:31 +0530

    Create Mandate_fields3.js

---
+++ Company_Universe/Mandate_fields3.js
@@ -0,0 +1,66 @@
+// Company Universe - Created or Edited - Validations on form submission - Mandate fields
+// Not required
+// if(input.Marketing_Capitalization_Rs_Cr == "Yes")
+// {
+// 	if(isNull(input.Marketing_Capitalization_Amount))
+// 	{
+// 		alert "Please Enter Marketing Capitalization Amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.Free_flot_market_capitalization_Rs_Cr == "Yes")
+// {
+// 	if(isNull(input.Free_flot_marketing_capitalization_amount))
+// 	{
+// 		alert "Please Enter Free flot marketing capitalization amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.Latest_Price_Default == "Yes")
+// {
+// 	if(isNull(input.Latest_price_amount))
+// 	{
+// 		alert "Please Enter Latest price amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.week_High_Date == "Yes")
+// {
+// 	if(isNull(input.week_High_Date_amount))
+// 	{
+// 		alert "Please Enter 52 week High date amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.week_Low_Date == "Yes")
+// {
+// 	if(isNull(input.week_Low_Date_amount))
+// 	{
+// 		alert "Please Enter 52 week Low date amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.days_average_daily_volumes == "Yes")
+// {
+// 	if(isNull(input.days_average_daily_volumes_amount))
+// 	{
+// 		alert "Please Enter 30 days average daily volumes amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.months_average_daily_volumes == "Yes")
+// {
+// 	if(isNull(input.months_average_daily_volumes_amount))
+// 	{
+// 		alert "Please Enter 3 months average daily volumes amount";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.months_average_daily_volumes1 == "Yes")
+// {
+// 	if(isNull(input.month_average_daily_volume_amount))
+// 	{
+// 		alert "Please Enter 6 months average daily volume amount";
+// 		cancel submit;
+// 	}
+// }
