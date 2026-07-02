ded56751825db89fdca2341f91409924a01c0689
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 13:28:20 +0530

    Create FI_AB_Delete_Completed_Me.js

---
+++ Morning_Meeting/Schedules/FI_AB_Delete_Completed_Me.js
@@ -0,0 +1,11 @@
+// On 10-Dec-2025 - At 11:31:00 - FI_AB Delete Completed Meeting Drafts
+// Scheduler disabled as per client request
+// meetingIds = Meeting_Details[Meeting_Date == zoho.currentdate].ID.getAll();
+// // info meetingIds;
+// for each  m in meetingIds
+// {
+// 	for each  rec in Morning_Notes[Meeting_Detail == m && Record_Status1 == "Draft"]
+// 	{
+// 		delete from Morning_Notes[ID == rec.ID];
+// 	}
+// }
