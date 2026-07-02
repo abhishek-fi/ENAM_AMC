d017b4ccbac290da7dfcbd0533a24c4f315780b1
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 12:22:32 +0530

    Create daily_meeting.js

---
+++ Morning_Meeting/Schedules/daily_meeting.js
@@ -0,0 +1,8 @@
+// On 30-Jul-2025 Daily - At 11:30:00 - daily meeting
+/* tomorrow = zoho.currentdate.addDay(1);
+ 				 														day = tomorrow.getDayOfWeek();
+ 				 														if(day == 1 || day == 7)
+ 				 														{
+ 				 															thisapp.create_meeting_with_attendance();
+ 				 														} */
+thisapp.create_meeting_with_attendance();
