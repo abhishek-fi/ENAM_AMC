41d637f4ce1d6d64c1368a0ed6ad38b67464d5a8
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 02:06:45 +0530

    Create Mandate_fields2.js

---
+++ Company_Universe/Mandate_fields2.js
@@ -0,0 +1,26 @@
+// Company Universe - Created or Edited - Validations on form submission - Mandate fields
+// Not required, handled in widget
+// if(input.ENAM_Investment_Philoshophy == "Partial")
+// {
+// 	if(isNull(input.What_of_businesses_are_non_compliant_with_ENAM_Philosophy_or_state_any_other_reason))
+// 	{
+// 		alert "Please enter What % of businesses are non compliant with ENAM Philosophy or state any other reason!";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.Any_Other_Information == "Yes")
+// {
+// 	if(isNull(input.Additional_Information))
+// 	{
+// 		alert "Please Enter Additional Information";
+// 		cancel submit;
+// 	}
+// }
+// else if(input.months_average_daily_volumes == "Yes")
+// {
+// 	if(isNull(input.months_average_daily_volumes_amount))
+// 	{
+// 		alert "Please Enter months average daily volumes amount";
+// 		cancel submit;
+// 	}
+// }
