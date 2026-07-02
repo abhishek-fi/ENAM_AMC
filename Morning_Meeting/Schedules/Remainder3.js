00923bafe94bc4fd09c306c9d729312fff4d02e2
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 12:51:32 +0530

    Create Remainder3.js

---
+++ Morning_Meeting/Schedules/Remainder3.js
@@ -0,0 +1,8 @@
+// On 31-Jul-2025 - At 09:15:00 Daily - Remainder3
+// Scheduler disabled as per client request
+appUri = zoho.appuri;
+if(appUri.contains("environment"))
+{
+	return;
+}
+thisapp.getthirdremainder();
