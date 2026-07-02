476849ea3191d07ebef0fbd37b8b1b9b8f6e84a0
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 01:58:14 +0530

    Create FI_AB_Hide_Fields.js

---
+++ Company_Universe/FI_AB_Hide_Fields.js
@@ -0,0 +1,8 @@
+// Company Universe - Created or Edited - Load of the form - FI_AB Hide Fields
+hide Detailed_Presentation_Added;
+hide Pre_assessment_Presentation_Added;
+hide Focus_List_In_Progress;
+hide Universe_Addition_In_Progress;
+hide Deletion_Process_Flow;
+// hide Dev_Section;
+hide Focus_Flow_Comments;
