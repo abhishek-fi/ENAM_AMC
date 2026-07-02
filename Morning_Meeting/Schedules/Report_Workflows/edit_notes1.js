a3a56d8c9c519dbb7af703a2a45a14abf8c94cc0
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 16:05:04 +0530

    Create edit_notes1.js

---
+++ Morning_Meeting/Schedules/Report_Workflows/edit_notes1.js
@@ -0,0 +1,2 @@
+// Morning Meeting Notes - In Morning Meeting Notes Report - On click of action item - edit_notes -- Not in use
+openUrl("https://creatorapp.zoho.in" + zoho.appuri + "#Page:Edit_Notes?recid=" + input.ID,"new window");
