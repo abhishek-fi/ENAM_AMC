e47ebe1eaf43b5177e916d9acbb0b996903563b7
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 12:23:29 +0530

    Update FI_AB_Revoke_edit_access_.js

--- Morning_Meeting/Schedules/FI_AB_Revoke_edit_access_.js
+++ Morning_Meeting/Schedules/FI_AB_Revoke_edit_access_.js
@@ -1,3 +1,3 @@
-// Morning Notes - Based on Edit Access Date Time - FI_AB Revoke edit access after 24 hours
+// Morning Notes - Based on Edit Access Date Time: after 24 hours - FI_AB Revoke edit access after 24 hours
 
 input.Edit_Access = false;
