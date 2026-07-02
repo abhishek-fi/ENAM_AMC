4370a9e88f327214e0261aedf04d882dea43d559
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 17:56:00 +0530

    Create FI_AB_Mandate_fields_duri2.js

---
+++ Company_Universe/FI_AB_Mandate_fields_duri2.js
@@ -0,0 +1,13 @@
+// Company Universe - Edited - Validations on form submission - FI_AB Mandate fields during transition L1 Review -- Disabled
+// if(input.Manual_Edit_Flag == false && Universe_Addition_In_Progress == true && Custom_Stage == "Universe Addition Initiated")
+// {
+// 	if(input.Screener.isEmpty() || Pre_assessment_Presentation.isEmpty() || Segmental_revenues.isEmpty() || Revenue_drivers.isEmpty() || Margin_drivers.isEmpty() || Balance_Sheet_Structure.isEmpty() || Capital_allocation.isEmpty() || Shareholding_pattern.isEmpty() || Liquidity_details.isEmpty() || Any_Other_Information.isEmpty() || input.FM_1_Name == null || input.FM_2_name == null || input.FM_3_Name == null)
+// 	{
+// 		alert "Please enter FM Names and Pre-assesment Details.";
+// 		cancel submit;
+// 	}
+// }
+// else
+// {
+// 	input.Manual_Edit_Flag = false;
+// }
