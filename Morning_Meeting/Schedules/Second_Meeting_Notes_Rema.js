0032d5aab009b525ef2ca796028b8181b7c4b06b
abhishek-fi <abhishek@fristinetech.com>
2026-07-02 13:24:05 +0530

    Create Second_Meeting_Notes_Rema.js

---
+++ Morning_Meeting/Schedules/Second_Meeting_Notes_Rema.js
@@ -0,0 +1,8 @@
+// On 05-Aug-2025 - At 11:15:00 Daily - Second Meeting Notes Remainder
+// Scheduler disabled as per client request
+appUri = zoho.appuri;
+if(appUri.contains("environment"))
+{
+	return;
+}
+thisapp.thirdMeetingNotesremainder();
