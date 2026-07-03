054bdabd4d449b29cd6c77829894206baa0bea9d
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 18:06:19 +0530

    Create Open_Widget5.js

---
+++ Company_Meeting/Report_Workflows/Open_Widget5.js
@@ -0,0 +1,2 @@
+// Company Meeting - In My Company Meetings - On click of action item: Edit - Open Widget
+openUrl("https://creatorapp.zoho.in" + zoho.appuri + "#Page:Company_Meeting1?recordId=" + input.ID,"new window");
