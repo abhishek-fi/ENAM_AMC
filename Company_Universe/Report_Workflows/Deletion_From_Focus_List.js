03262ed39dcc5e697d9a4c620b96296cc16fefa5
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:17:26 +0530

    Create Deletion_From_Focus_List.js

---
+++ Company_Universe/Report_Workflows/Deletion_From_Focus_List.js
@@ -0,0 +1,2 @@
+// Company Universe - In My Focus List - On click of action item: Deletion From Focus List - Deletion_From_Focus_List
+openUrl("#Form:Deletion_from_focus_list?Record_ID=" + input.ID,"popup window");
