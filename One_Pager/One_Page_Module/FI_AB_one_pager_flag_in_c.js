219f649aa629438448c374b024179ea5b8042cd3
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 16:15:38 +0530

    Create FI_AB_one_pager_flag_in_c.js

---
+++ One_Pager/One_Page_Module/FI_AB_one_pager_flag_in_c.js
@@ -0,0 +1,3 @@
+// One Page Module -Created - Successful form submission - FI_AB one pager flag in company record
+compRec = Company_Universe[ID == input.Company_Name];
+compRec.Current_Month_One_Pager=true;
