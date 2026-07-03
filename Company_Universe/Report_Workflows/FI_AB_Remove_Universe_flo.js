64dc44d762de0a02f575674a5e5954e0f59bcf56
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:18:29 +0530

    Update FI_AB_Remove_Universe_flo.js

--- Company_Universe/Report_Workflows/FI_AB_Remove_Universe_flo.js
+++ Company_Universe/Report_Workflows/FI_AB_Remove_Universe_flo.js
@@ -1,2 +1,2 @@
-// Company Universe - In Universe Companies List - On click of action item - FI_AB Remove Universe flow start -- not in use
+// Company Universe - In Universe Companies List - On click of action item: Remove From Universe - FI_AB Remove Universe flow start -- not in use
 input.Remove_From = "Universe";
