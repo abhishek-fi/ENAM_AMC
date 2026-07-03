01b8d16f8829af91646013a936b327c994aec4ed
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 16:47:29 +0530

    Create Open_one_pager_with_cloni.js

---
+++ One_Pager/Report_Workflows/Open_one_pager_with_cloni.js
@@ -0,0 +1,4 @@
+// One Page Module - In My One Pagers - On click of action item: Clone - Open one pager with cloning params
+baseUrl = "#Page:One_Pager";
+url = baseUrl + "?create_new=true&recordid=" + input.ID;
+openUrl(url,"new window","successive=true");
