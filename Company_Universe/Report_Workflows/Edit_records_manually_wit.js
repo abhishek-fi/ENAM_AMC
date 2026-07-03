00f3f4a5a7a8c9c3e78512b9ee1f8f627396c8f9
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:01:00 +0530

    Create Edit_records_manually_wit.js

---
+++ Company_Universe/Report_Workflows/Edit_records_manually_wit.js
@@ -0,0 +1,2 @@
+// Company Universe - In My Companies - On click of action item - Edit records manually with edit url -- not in sue
+openUrl("https://creatorapp.zoho.in" + zoho.appuri + "Company_Universe/record-edit/Company_Universe_Report/" + input.ID + "?Manual_Edit_Flag=true","new window");
