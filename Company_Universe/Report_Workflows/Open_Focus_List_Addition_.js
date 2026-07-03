a88c7bbb4c30bbc83c070b300abafa3c39c48581
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:26:41 +0530

    Create Open_Focus_List_Addition_.js

---
+++ Company_Universe/Report_Workflows/Open_Focus_List_Addition_.js
@@ -0,0 +1,2 @@
+// Company Universe - In My Companies Universe - On click of action item: Add to Focus List - Open Focus List Addition Page
+openUrl("https://creatorapp.zoho.in" + zoho.appuri + "#Page:Focus_List_Addition?companyId=" + input.ID,"new window");
