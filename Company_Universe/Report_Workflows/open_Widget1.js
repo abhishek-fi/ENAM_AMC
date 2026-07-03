14816a97f99918e192b384ebb1f4c6bc912ebc16
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:08:24 +0530

    Create open_Widget1.js

---
+++ Company_Universe/Report_Workflows/open_Widget1.js
@@ -0,0 +1,2 @@
+// Company Universe - In All Companies - On click of action item: Add to Universe - open Widget
+openUrl("https://creatorapp.zoho.in" + zoho.appuri + "#Page:Universe_Addition?recid=" + input.ID,"new window");
