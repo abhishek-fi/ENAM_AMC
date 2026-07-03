21d16f579388831a728734dff98636e58b7c5ab1
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 16:18:05 +0530

    Create Hide_Fields.js

---
+++ One_Pager/One_Page_Module/Hide_Fields.js
@@ -0,0 +1,40 @@
+// One Page Module - Created or Edited - Load of the form - Hide Fields
+if(input.Block_6_Option == "Written Content")
+{
+	hide Block_6_Image;
+}
+else if(input.Block_6_Option == "Image Upload")
+{
+	hide Block_6_Content;
+}
+else
+{
+	hide Block_6_Image;
+	hide Block_6_Content;
+}
+if(input.Block_5_Option == "Written Content")
+{
+	hide Block_5_Image;
+}
+else if(input.Block_5_Option == "Image Upload")
+{
+	hide Block_5_Content;
+}
+else
+{
+	hide Block_5_Image;
+	hide Block_5_Content;
+}
+if(input.Block_9_Option == "Written Content")
+{
+	hide Block_9_Image;
+}
+else if(input.Block_9_Option == "Image Upload")
+{
+	hide Block_9_Content;
+}
+else
+{
+	hide Block_9_Image;
+	hide Block_9_Content;
+}
