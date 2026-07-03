85646be8242aee2c079d5f5e40d46f84b6e5d974
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 16:41:59 +0530

    Create Open_Widget3.js

---
+++ One_Pager/Report_Workflows/Open_Widget3.js
@@ -0,0 +1,2 @@
+// One Page Module - In One Page Module Report - On click of action item: Edit One Pager - Open Widget
+openUrl("#Page:One_Pager?recordid=" + input.ID,"new window","successive=true");
