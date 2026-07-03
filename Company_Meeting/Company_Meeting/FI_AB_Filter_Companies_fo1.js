12d6b1193ba158a0a9150013b01629490b6ccc2e
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 17:26:02 +0530

    Create FI_AB_Filter_Companies_fo1.js

---
+++ Company_Meeting/Company_Meeting/FI_AB_Filter_Companies_fo1.js
@@ -0,0 +1,10 @@
+// Company Meeting - Created or Edited - Load of the form - FI_AB Filter Companies for User Profiles
+userRec = User_Master[Email_Id == zoho.loginuserid];
+currValue = input.Company_Name;
+if(userRec.Profile == "Analyst")
+{
+	compList = Company_Universe[Analyst2 == userRec.ID].ID.getAll();
+	input.Company_Name:ui.add(compList);
+	input.Company_Name = currValue;
+}
+input.Company_Name = currValue;
