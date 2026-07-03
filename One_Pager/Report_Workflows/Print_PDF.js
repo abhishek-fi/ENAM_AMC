ea2df6b3e79da150bc39a83633ddc7e9ce9186da
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 16:45:08 +0530

    Create Print_PDF.js

---
+++ One_Pager/Report_Workflows/Print_PDF.js
@@ -0,0 +1,3 @@
+// One Page Module - In One Page Module Report -  - On click of action item: Preview - Print PDF -- to preview one pager
+url = "https://creatorapp.zoho.in" + zoho.appuri + "#Page:Print_One_Pager?recordId=" + input.ID;
+openURL(url,"new window","successive=true");
