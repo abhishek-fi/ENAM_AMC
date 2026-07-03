f829a27ba8c567a3fbd0264113df3469f3f65756
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 14:02:23 +0530

    Create FI_AB_Edit_Record_Manuall.js

---
+++ Company_Universe/Report_Workflows/FI_AB_Edit_Record_Manuall.js
@@ -0,0 +1,2 @@
+// Company Universe - In My Companies - On click of action item - FI_AB Edit Record Manually -- not in use
+openUrl("#Form:Company_Universe?recLinkID=" + input.ID + "&viewLinkName=Company_Universe_Report","new window");
