17b3ca41ff50502efe40a6052163f8ebc1ad2639
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 15:00:10 +0530

    Create View_only_the_Meeting_Not.js

---
+++ Morning_Meeting/Meeting_Notes_Edit_Request/View_only_the_Meeting_Not.js
@@ -0,0 +1,10 @@
+// Meeting Notes Edit Request - Created or Edited - Load of the form - View only the Meeting Notes Edit Request Form
+alert "You do not have access to create record";
+disable Meeting_Notes;
+disable Company_Name;
+disable Approved;
+disable Edit_Request_Raise_Time;
+disable Analyst_Name;
+disable Approve_Comment;
+disable Rejection;
+disable Meet_Note;
