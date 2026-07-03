528e231b427bf28eb3607ec9c5edb836e7304a5a
abhishek-fi <abhishek@fristinetech.com>
2026-07-03 17:49:46 +0530

    Create Show_Cancellation_Reason.js

---
+++ Company_Meeting/Company_Meeting/Show_Cancellation_Reason.js
@@ -0,0 +1,9 @@
+// Company Meeting - Created or Edited - User input of Status - Show Cancellation Reason
+if(input.Status == "Cancelled")
+{
+	show Cancellation_Reason;
+}
+else
+{
+	hide Cancellation_Reason;
+}
